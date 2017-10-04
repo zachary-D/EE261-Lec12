8415302dda50b55d731efe457d53db2f46565fde
Unknown <zacharyd1@live.com>
2017-10-04 10:33:26 -0400

    Completed converter

--- Lecture 12/Lecture 12/Source.cpp
+++ Lecture 12/Lecture 12/Source.cpp
@@ -4,9 +4,13 @@
 
 int main()
 {
+	double mph = 50;
+	const double conversionFactor = (5280 * .3048) / (60 * 60) ;
+	double mps = mph*conversionFactor;
 
+	std::cout << mps << std::endl;
 
-
+	std::cin.get();
 
 	return 0;
 }
